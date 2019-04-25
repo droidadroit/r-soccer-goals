f674a821ab49f42b036e1ab530fcd35c66503a73
droidadroit <yashu95rdy@tamu.edu>
2019-04-25 00:05:48 -0500

    added stickied mirrors

--- js/app.js
+++ js/app.js
@@ -3,16 +3,23 @@ import * as utils from './utils.js';
 
 let getCommentsLink = (link) => `https://reddit.com${link}`
 
+let _getMirrors = () => {
+    return ['mirror1', 'mirror2', 'mirror3', 'mirror4', 'mirror5', 'mirror6'];
+}
+
 let processData = (rawData) => {
     let filtered_posts = rawData.data.children.filter(child => regexes.some(regex => regex.test(child.data.title)));
     return filtered_posts.map(child => {
         return {
-            title: child.data.title,
-            comments: getCommentsLink(child.data.permalink), 
-            link: child.data.url,
-            time: utils.getDate(child.data.created * 1000),
-            domain: child.data.domain
+                title: child.data.title,
+                comments: getCommentsLink(child.data.permalink), 
+                link: child.data.url,
+                time: utils.getDate(child.data.created * 1000),
+                domain: child.data.domain,
+                id: child.data.id,
+                mirrors: null
         };
+
     });
 };
 
@@ -25,6 +32,38 @@ let getQueryForRedditApi = (searchQuery, sortBy) => {
     url += `&sort=${sortBy}`
     url += `&limit=1000`;
     return encodeURI(url);
+};
+
+let processBodyHtml = (html) => {
+    return html.replace(/&lt;/g, "<").replace(/&gt;/g, ">");
+};
+
+let getMirrors = (post_id) => {
+    return axios
+        .get(encodeURI(`https://api.reddit.com/r/soccer/comments/${post_id}?depth=1&limit=1&sort=top`))
+        .then(response => {
+            let parent_id = response.data[1].data.children[0].data.id;
+            return axios
+                .get(encodeURI(`https://api.reddit.com/r/soccer/comments/${post_id}?depth=2&sort=top&comment=${parent_id}`))
+                .then(response => {
+                    let replies = response.data[1].data.children[0].data.replies.data.children; // FIXME: handle when there are no replies to a comment
+                    let body_htmls = replies.map(reply => reply.data.body_html);
+                    let mirrors = [];
+                    let htmls = body_htmls.map(body_html => processBodyHtml(body_html));
+                    mirrors = [];
+                    htmls.forEach(html => {
+                        html.replace(/[^<]*(<a href="([^"]+)">([^<]+)<\/a>)/g, function() {
+                            mirrors.push(arguments[2]);
+                        });
+                    });
+                    return mirrors;
+                });
+            });
+};
+
+let assignMirrors = (mirrors, posts, id) => {
+    let obj = posts.filter(post => post.id === id);
+    obj[0]['mirrors'] = mirrors;
 };
 
 const app = new Vue({
@@ -64,7 +103,14 @@ const app = new Vue({
                 })
                 .catch(error => console.log(error));
         },
-
-        openLink: utils.openInNewTab
+        openLink: utils.openInNewTab,
+        loadMirrors:function(id) {
+            let vm = this;
+            if (vm.posts.filter(post => post.id === id)[0].mirrors === null) {
+                getMirrors(id).then(data => {
+                    assignMirrors(data, vm.posts, id);
+                });
+            }
+        }
     }
 });
